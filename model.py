9f0361ec733b9079429558f0602dee844077bc56
Binbin Dong <binbin.dong1994@gmail.com>
2021-04-20 05:04:06 +0200

    fixed typo

--- model.py
+++ model.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 import tensorflow.keras as keras
-import tensorflow_addons as tfa
 
-from DL1_model.models.maxout_layers import Maxout1D
+import sys
+sys.path.append('DL1_model/')
+from models.maxout_layers import Maxout1D
 
 def DL1Model(InputShape, h_layers, lr=0.01, drops=None, dropout=True):
 	In = keras.layers.Input(shape=[InputShape,])
 	x = In
-	x = Maxout1D(h_layers[0,],25)(x)
+	x = Maxout1D(h_layers[0],25)(x)
 	x = keras.layers.BatchNormalization()(x)
 	x = keras.layers.Dropout(drops[0])(x, training=dropout)
 	for i, h in enumerate(h_layers[1:]):
