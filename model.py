78e1ff94d4e943cafdf38c8d74c9cec1b755f452
Binbin Dong <binbin.dong1994@gmail.com>
2021-04-23 06:26:06 +0200

    added official and private DL1 model

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('DL1_model/')
 from models.maxout_layers import Maxout1D
 
-def DL1Model(InputShape, h_layers, lr=0.01, drops=None, dropout=True):
+def official_DL1Model(InputShape, h_layers, lr=0.01, drops=None, dropout=True):
 	In = keras.layers.Input(shape=[InputShape,])
 	x = In
 	x = Maxout1D(h_layers[0],25)(x)
@@ -18,9 +18,9 @@ def DL1Model(InputShape, h_layers, lr=0.01, drops=None, dropout=True):
 			x = keras.layers.Dropout(drops[i+1])(x, training=dropout)
 			continue
 
-		x = keras.layers.Dense(h, activation="linear",kernel_initializer='glorot_uniform')(x)
+		x = keras.layers.Dense(h, activation="relu",kernel_initializer='glorot_uniform')(x)
 		x = keras.layers.BatchNormalization()(x)
-		x = keras.layers.Activation("relu")(x)
+		#x = keras.layers.Activation("relu")(x)
 		x = keras.layers.Dropout(drops[i+1])(x, training=dropout)
 
 	predictions = keras.layers.Dense(3, activation='softmax', kernel_initializer='glorot_uniform')(x)
@@ -30,8 +30,31 @@ def DL1Model(InputShape, h_layers, lr=0.01, drops=None, dropout=True):
 	model_optimizer = keras.optimizers.Adam(lr=lr)
 
 	model.compile(
-			loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+			loss = 'categorical_crossentropy',
 			optimizer=model_optimizer,
 			metrics=['accuracy'])
 
 	return model
+
+
+def private_DL1Model(InputShape, h_layers, lr=0.01, drops=None, dropout=True, batch_size=3000):
+	In = keras.layers.Input(shape=[InputShape,])
+	x = In
+	for i, h in enumerate(h_layers[:]):
+		x = keras.layers.Dense(h, activation="linear",kernel_initializer='glorot_uniform')(x)
+		x = keras.layers.BatchNormalization()(x)
+		x = keras.layers.Activation("relu")(x)
+		x = keras.layers.Dropout(drops[i])(x, training=dropout)
+
+	predictions = keras.layers.Dense(3, activation='softmax', kernel_initializer='glorot_uniform')(x)
+
+	model = keras.models.Model(inputs=In, outputs=predictions)
+
+	model_optimizer = keras.optimizers.Adam(lr=lr)
+
+	model.compile(
+			loss = 'categorical_crossentropy',
+			optimizer=model_optimizer,
+			metrics=['accuracy'])
+
+	return model, batch_size
