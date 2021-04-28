5821ab0d693ce609b87ef8c5438923d4da2c5c63
Binbin Dong <binbin.dong1994@gmail.com>
2021-04-29 00:24:58 +0200

    minor change

--- evaluate.py
+++ evaluate.py
@@ -5,6 +5,7 @@ import model
 import tensorflow as tf
 from itertools import compress
 import timeit
+import sys
 
 from keras.models import load_model
 from keras.utils import np_utils
@@ -16,17 +17,22 @@ parser.add_argument('-o', '--output', type=str,
 		default="output/DL1_test.h5",
 		help='''set the output file path, where the results are stored''')
 
-parser.add_argument('-i', '--validation_file', type=str,
+parser.add_argument('-i', '--input_file', type=str,
 		default="input/MC16D_ttbar-test-ujets.h5",
 		help='Set name of preprocessed input validation file')
 parser.add_argument('-w', '--WP', type=int,
 		default=85,
 		help='Choose btagging WP')
+parser.add_argument('-l', '--label', type=int,
+		default=0,
+		help='choose the flavour of jets want to be processed')
 
 args = parser.parse_args()
 
 n_predictions = 10000 
 fc = 0.08 #0.18 # c-jet fraction
+
+publicDL1 = True
 
 DL1_cut = 0.46 # DL1 cut to each WP
 if(args.WP == 85):
@@ -40,14 +46,17 @@ elif(args.WP == 60):
 else:
 	print('Available WP: 60, 70, 77, 85. Please choose one WP among them.')
 	print('for more details, check https://twiki.cern.ch/twiki/bin/view/AtlasProtected/BTaggingBenchmarksRelease21#DL1_tagger')
+	sys.exit()
 
-f = h5py.File(args.validation_file, 'r')
-print('loading dataset: ', args.validation_file)
+try:
+	f = h5py.File(args.input_file, 'r')
+	print('loading dataset: ', args.input_file)
+except FileNotFoundError:
+	print('Oooops! No such file!')
 
-publicDL1 = True
 if publicDL1:
-	test_model =  tf.keras.models.load_model('DL1_model/DL1_AntiKt4EMTopo')
-	test_model_Dropout =  tf.keras.models.load_model('DL1_model/DL1_AntiKt4EMTopo_dropout')
+	test_model = tf.keras.models.load_model('DL1_model/DL1_AntiKt4EMTopo')
+	test_model_Dropout = tf.keras.models.load_model('DL1_model/DL1_AntiKt4EMTopo_dropout')
 	test_model_Dropout.summary()
 else:
 	InputShape = 44
@@ -59,39 +68,44 @@ else:
 	test_model.load_weights('models/DL1_hybrid_2M_b3000_e1.h5')
 	test_model_Dropout.load_weights('models/DL1_hybrid_2M_b3000_e1.h5')
 
-X_test = f['X_test'][0:100]
-#Y_test = f['Y_test'][:]
-labels = f['labels'][0:100]
+X_test = f['X_test'][0:1000]
+labels = f['labels'][0:1000]
 
 ## selecte light jets, in future can selecte this at file preparation stage
-light_X = np.array(list(compress(X_test, labels==0)))
-#light_Y = np.array(list(compress(Y_test, labels==0)))
-print('Progress -- light jets selected')
+select_jets_X = np.array(list(compress(X_test, labels==args.label)))
+select_jets_label = np.array(list(compress(labels, labels==args.label)))
+print('Progress -- jets selected')
 
 ## evaluation with dropout disabled
-nodropout = test_model.predict(light_X)
+nodropout = test_model.predict(select_jets_X)
 nodropout_l = nodropout[:,0]
 nodropout_b = nodropout[:,1]
 nodropout_c = nodropout[:,2]
 nodropout_DL1 = np.log(nodropout_b/(fc*nodropout_c+(1-fc)*nodropout_l))
-print(nodropout)
 
 ## get mis-tagged light jets
-mistag_light_X = np.array(list(compress(light_X, nodropout_DL1>DL1_cut)))
-#mistag_light_Y = np.array(list(compress(light_Y, nodropout_DL1>DL1_cut)))
-print('Progress -- evaluted light jets with dropout disabled')
+btagged_X = np.array(list(compress(select_jets_X, nodropout_DL1>DL1_cut)))
+print('Progress -- evaluted jets with dropout disabled')
 
-## evaluate mis-tagged light jets with dropout enabled
+## evaluate b-tagged jets with dropout enabled
 init = timeit.default_timer()
-test_data = mistag_light_X.tolist() * n_predictions
-print(len(test_data))
+test_data = btagged_X.tolist() * n_predictions
 dropout = test_model_Dropout.predict(test_data)
 print('Progress -- evaluated mis-tagged light jets with dropout enabled')
 
 dropout_DL1 = np.log(dropout[:,2] / (fc*dropout[:,1] + (1-fc)*dropout[:,0]))
 print('Progress -- output processed, saving output.')
+
 final = timeit.default_timer()
 print('time used to evalute jets: {}s'.format(final-init))
+
+print('total processed jets: {}'.format(select_jets_X.size / 41.))
+print('{} jets tagged as b-jets'.format(btagged_X.size / 41.))
+if (args.label==5):
+	print('b-tagging efficiency: {} %'.format(btagged_X.size / select_jets_X.size * 100))
+else:
+	print('mis-tag rate: {} %'.format(btagged_X.size / select_jets_X.size * 100))
+
 
 fout = h5py.File(args.output, 'w')
 fout.create_dataset('l_score_nodropout', data=np.array(list(compress(nodropout_l, nodropout_DL1>DL1_cut))))
