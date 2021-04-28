490ef1ca6efe59f0e61ec6cdd025d1341ab6d0eb
Binbin Dong <binbin.dong1994@gmail.com>
2021-04-28 03:42:31 +0200

    removed offical DL1 model

--- model.py
+++ model.py
@@ -5,38 +5,6 @@ import sys
 sys.path.append('DL1_model/')
 from models.maxout_layers import Maxout1D
 
-def official_DL1Model(InputShape, h_layers, lr=0.01, drops=None, dropout=True):
-	In = keras.layers.Input(shape=[InputShape,])
-	x = In
-	x = Maxout1D(h_layers[0],25)(x)
-	x = keras.layers.BatchNormalization()(x)
-	x = keras.layers.Dropout(drops[0])(x, training=dropout)
-	for i, h in enumerate(h_layers[1:]):
-		if i==4:
-			x = Maxout1D(h, 25)(x)
-			x = keras.layers.BatchNormalization()(x)
-			x = keras.layers.Dropout(drops[i+1])(x, training=dropout)
-			continue
-
-		x = keras.layers.Dense(h, activation="relu",kernel_initializer='glorot_uniform')(x)
-		x = keras.layers.BatchNormalization()(x)
-		#x = keras.layers.Activation("relu")(x)
-		x = keras.layers.Dropout(drops[i+1])(x, training=dropout)
-
-	predictions = keras.layers.Dense(3, activation='softmax', kernel_initializer='glorot_uniform')(x)
-
-	model = keras.models.Model(inputs=In, outputs=predictions)
-
-	model_optimizer = keras.optimizers.Adam(lr=lr)
-
-	model.compile(
-			loss = 'categorical_crossentropy',
-			optimizer=model_optimizer,
-			metrics=['accuracy'])
-
-	return model
-
-
 def private_DL1Model(InputShape, h_layers, lr=0.01, drops=None, dropout=True, batch_size=3000):
 	In = keras.layers.Input(shape=[InputShape,])
 	x = In
