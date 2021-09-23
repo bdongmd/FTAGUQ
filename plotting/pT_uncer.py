bc2793e930d5289d90118dc93276e9a772a18ad2
Binbin Dong <binbin.dong1994@gmail.com>
2021-09-24 01:15:29 +0200

    plot eff as a func of pt

--- plotting/pT_uncer.py
+++ plotting/pT_uncer.py
@@ -1,10 +1,11 @@
 import h5py
 import numpy as np
 import os
-import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import plot_lib
 
 ttbar_bins = [10,20,30,45,60,75,100,250]
-Zprime_bins = [500, 600, 700, 800, 900, 1000, 1100, 1250, 1400, 1550, 1750, 2000, 2250, 2500, 2750, 3000]
+Zprime_bins = [400, 500, 600, 700, 800, 900, 1000, 1100, 1250, 1400, 1550, 1750, 2000, 2250, 2500, 2750, 3000]
 ## the Zprime binning is only for extrapolation uncertainty range.
 
 bins = Zprime_bins
@@ -13,7 +14,6 @@ DL1_cut = 2.195
 
 shift = 602457.2480418527
 scale = 1031525.5408820781
-
 def pT_scale(pT_scaled):
 	''' scale the scaled pT back to GeV'''
 	pT = np.array(pT_scaled)*scale + shift
@@ -38,19 +38,35 @@ def get_eff_Dropout(pT, DL1_score):
 	hist_effs = []
 	for i in range(len(DL1_score[0])):
 		hist_effs.append(get_eff_hist(pT, DL1_score[:,i]))
-	print(hist_effs)
-	return (np.median(hist_effs, axis=0).flatten(), np.std(hist_effs, axis=0).flatten())
+	return (hist_effs, np.median(hist_effs, axis=0).flatten(), np.std(hist_effs, axis=0).flatten())
 
 
-
-f = h5py.File('/eos/user/b/bdong/DUQ/UmamiTrain/DL1r-PFlow_new-taggers-stats-22M/Zprime/Zprime_evalauted_bjets_0_to_50k.h5', 'r')
-scaled_pt = f['scaled_pt'][:]
-DL1_score_noDropout = f['DL1_score_noDropout'][:]
-DL1_score = f['DL1_score'][:]
-f.close()
+directory = "/eos/user/b/bdong/DUQ/UmamiTrain/DL1r-PFlow_new-taggers-stats-22M/tmp/"
+for i in os.listdir(directory):
+	if i.endswith('.h5'):
+		with h5py.File(os.path.join(directory, i)) as f:
+			print("adding file: {}".format(i))
+			try:
+				scaled_pt = np.concatenate([f['scaled_pt'][:], scaled_pt])
+				DL1_score_noDropout = np.concatenate([f['DL1_score_noDropout'][:], DL1_score_noDropout])
+				DL1_score = np.concatenate([f['DL1_score'][:], DL1_score])
+			except NameError:
+				scaled_pt = np.concatenate([f['scaled_pt'][:]])
+				DL1_score_noDropout = np.concatenate([f['DL1_score_noDropout'][:]])
+				DL1_score = np.concatenate([f['DL1_score'][:]])
+		f.close()
 
 pt = pT_scale(scaled_pt) 
+DL1r_bins = np.linspace(-5, 15, 50).tolist()
+
 eff_noDropout = get_eff_hist(pt, DL1_score_noDropout)
-eff_Dropout, eff_sys = get_eff_Dropout(pt, DL1_score)
-print(eff_noDropout)
-print(eff_Dropout, eff_sys)
+hist_effs, eff_Dropout, eff_sys = get_eff_Dropout(pt, DL1_score)
+
+pdf = PdfPages("output/test.pdf")
+plot_lib.plot_DL1r_pT(pt, DL1_score_noDropout, bins, DL1r_bins, pdf)
+pT_bins = []
+for i in range(len(bins)-1):
+	plot_lib.plot_1d_eff(hist_effs[:,i], '[{}, {}] GeV'.format(bins[i], bins[i+1]), pdf)
+	pT_bins.append((bins[i], bins[i+1])/2.)
+plot_lib.plot_eff_pT_1d(pT_bins, eff_noDropout, eff_Dropout, eff_sys, pdf)
+pdf.close()
