799bf038cf2eeb942443e27b490dbe465287c514
Binbin Dong <binbin.dong1994@gmail.com>
2021-11-10 08:48:49 +0100

    cleaned up script

--- plotting/pT_uncer.py
+++ plotting/pT_uncer.py
@@ -6,6 +6,7 @@ import plot_lib
 from tqdm import tqdm
 from scipy import stats
 import pT_uncer_lib
+import sys
 
 ttbar_bins = [10,20,30,45,60,75,100,250, 400]
 Zprime_bins = [400, 500, 600, 700, 800, 900, 1000, 1100, 1250, 1400, 1550, 1750, 2000, 2250, 2500, 2750, 3000]
@@ -19,7 +20,9 @@ ubound = 0.841344746
 
 DL1_cut = 2.195
 
-directory = "/eos/user/b/bdong/DUQ/UmamiTrain/DL1r-PFlow_new-taggers-stats-22M/tmp/"
+print("Progress -- Loading input files")
+#directory = "/eos/user/b/bdong/DUQ/UmamiTrain/DL1r-PFlow_new-taggers-stats-22M/tmp/"
+directory = sys.argv[1]
 for i in os.listdir(directory):
 	if i.endswith('.h5'):
 		with h5py.File(os.path.join(directory, i)) as f:
@@ -37,6 +40,7 @@ for i in os.listdir(directory):
 		f.close()
 print("Progress -- Done file merging ")
 
+print("Progress -- Starting to remove bad DL1r values")
 new_significance_median = []
 for i in tqdm (range(len(DL1_score)), desc="processing jets"):
 	## bad value: -0.63315678
@@ -51,40 +55,30 @@ for i in tqdm (range(len(DL1_score)), desc="processing jets"):
 		new_significance_median.append((tmp_DL1median - DL1_cut) / np.sqrt((tmp_DL1median - CI[0])**2))
 	else:
 		new_significance_median.append((tmp_DL1median - DL1_cut) / np.sqrt((tmp_DL1median - CI[1])**2))
-	
 probability_median = stats.norm.cdf(new_significance_median)
 del new_significance_median
+print("Progress -- Done calcualting predicted score after removing vad values")
 
 print("Progress -- pT scaling")
 pt = pT_uncer_lib.pT_scale(scaled_pt) 
 del scaled_pt
 DL1r_bins = np.linspace(-5, 15, 50).tolist()
 
-print("Progress -- calculating efficiencies")
+print("Progress -- Calculating efficiencies")
 #### get efficiency with Dropout enabled
-hist_effs, eff_Dropout_DUQ, eff_Dropout, eff_sys = pT_uncer_lib.get_eff_Dropout(pt, DL1_score)
-eff_noDropout = pT_uncer_lib.get_eff_hist(pt, DL1_score_noDropout)
-
-#### get DL1 median value for each jet
-#DL1_median, DL1_mean = get_each_jet_median(DL1_score)
+hist_effs, eff_Dropout_DUQ, eff_Dropout, eff_sys = pT_uncer_lib.get_eff_Dropout(pt, DL1_score) ## efficiency with Dropout
+eff_noDropout = pT_uncer_lib.get_eff_hist(pt, DL1_score_noDropout) ## efficiency without Dropout
 del DL1_score
-#eff_Dropout_median = get_eff_hist(pt, DL1_median)
-#eff_Dropout_mean = get_eff_hist(pt, DL1_mean)
-#eff_Dropout_predicted = get_eff_Dropout_predicted(pt, probability_median) 
 print("eff no Dropout = {}".format(eff_noDropout))
 print("eff Dropout = {}".format(eff_Dropout_DUQ))
-#print("error = {}".format(eff_sys))
 
 print("Progress -- plotting")
-pdf = PdfPages("output/500567_8.pdf")
+pdf = PdfPages("output/{}.pdf".format(sys.argv[2]))
 plot_lib.plot_DL1r_pT(pt, DL1_score_noDropout, bins, DL1r_bins, pdf)
 pT_bins = []
 for i in range(len(bins)-1):
 	plot_lib.plot_1d_eff(np.array(hist_effs)[:,i], '[{}, {}] GeV'.format(bins[i], bins[i+1]), pdf)
 	pT_bins.append((bins[i]+bins[i+1])/2.)
 plot_lib.plot_eff_pT_1d(pT_bins, eff_noDropout, eff_Dropout_DUQ, eff_sys, pdf)
-#plot_lib.plot_eff_pT_1d(pT_bins, eff_noDropout, eff_Dropout_predicted, eff_sys, pdf)
-#plot_lib.plot_eff_pT_1d(pT_bins, eff_noDropout, eff_Dropout_median, eff_sys, pdf)
-#plot_lib.plot_eff_pT_1d(pT_bins, eff_noDropout, eff_Dropout_mean, eff_sys, pdf)
 pdf.close()
 
